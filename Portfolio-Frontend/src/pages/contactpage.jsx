ba14d175ab139b9b5fc0c1b0b748bfa34a40e3f5
Saimahmed <softwareengineer278@gmail.com>
2025-06-25 17:45:56 +0500

    feat(contact): add social links and improve styling
    Added email, LinkedIn, and GitHub links to contact info.
    Refined layout and polished styling for better visual clarity.

--- Portfolio-Frontend/src/pages/contactpage.jsx
+++ Portfolio-Frontend/src/pages/contactpage.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { ContactForm } from "../component/contactform.jsx";
-import "../styles/contact.css"; 
+import "../styles/contact.css";
 
 export function ContactPage() {
   return (
@@ -15,16 +15,34 @@ export function ContactPage() {
       <div className="contact-info-container">
         <h2>Contact Information</h2>
         <p>
-          <FontAwesomeIcon icon={faEnvelope} /> saim@gmail.com
+          <FontAwesomeIcon icon={faEnvelope} />{" "}
+          <a
+            href="mailto:softwareengineer278@gmail.com"
+            style={{ color: "#3b82f6", textDecoration: "underline" }}
+          >
+            softwareengineer278@gmail.com
+          </a>
         </p>
         <p>
           <FontAwesomeIcon icon={faPhone} /> +92 312 3456789
         </p>
         <p>
-          <FontAwesomeIcon icon={faGithub} /> github.com/saimdev
+          <FontAwesomeIcon icon={faGithub} />{" "}
+          <a
+            href="https://github.com/Saimahmed78"
+            style={{ color: "#3b82f6", textDecoration: "underline" }}
+          >
+            https://github.com/Saimahmed78
+          </a>
         </p>
         <p>
-          <FontAwesomeIcon icon={faLinkedin} /> linkedin.com/in/saim-ahmed
+          <FontAwesomeIcon icon={faLinkedin} />{" "}
+          <a
+            href="https://www.linkedin.com/in/saim-ahmed-722b802ba"
+            style={{ color: "#3b82f6", textDecoration: "underline" }}
+          >
+            https://www.linkedin.com/in/saim-ahmed-722b802ba
+          </a>
         </p>
       </div>
     </section>
