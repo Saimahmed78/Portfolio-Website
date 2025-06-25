df40b65280a6800b5148fdb9ec22a5af5f2b7405
Saimahmed <softwareengineer278@gmail.com>
2025-06-25 17:39:40 +0500

    feat(homepage): add profile image to assets
    - Added a new profile image in the assets folder and updated the homepage UI to display it prominently in the hero section.

--- Portfolio-Frontend/src/pages/home.jsx
+++ Portfolio-Frontend/src/pages/home.jsx
@@ -1,23 +1,27 @@
-import { Link } from "@tanstack/react-router";
 import { CTAButton } from "../component/buttons";
 import "../styles/home.css";
+import profileImg from "../assets/Profile Image.jpg";
+
 export function Mainpage() {
   return (
-    <>
-      <main className="hero">
-        <div className="hero-left">
-          <h1>I am MERN Stack Developer</h1>
-          <p>
-            Hi, I’m Saim Ahmed — a passionate MERN stack developer who loves
-            building clean, functional web apps. I enjoy solving real-world
-            problems through code and constantly push myself to learn and
-            improve."
-          </p>
+    <main className="hero">
+      <div className="hero-left">
+        <h1>I am MERN Stack Developer</h1>
+        <p>
+          Hi, I’m Saim Ahmed — a passionate MERN stack developer who loves
+          building clean, functional web apps. I enjoy solving real-world
+          problems through code and constantly push myself to learn and
+          improve.
+        </p>
+        <CTAButton className="cta-button" to="/project">Project</CTAButton>
+        <CTAButton className="cta-button" to="/about">About me</CTAButton>
+      </div>
 
-          <CTAButton to="/project">Project</CTAButton>
-          <CTAButton to="/about">About me</CTAButton>
+      <div className="hero-right">
+        <div className="profile-img-wrapper">
+          <img src={profileImg} alt="Saim Ahmed" />
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
