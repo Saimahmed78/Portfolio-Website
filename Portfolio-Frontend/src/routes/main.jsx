16108d5de687c2c6b7c7195f4c5bea03f3a08b4f
Saimahmed <softwareengineer278@gmail.com>
2025-06-25 17:42:10 +0500

    delete useless files

--- Portfolio-Frontend/src/routes/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Mainpage } from "../pages/home";
-
-export const Route = createFileRoute("/main")({
-  component: Mainpage,
-});
-
-
--- Portfolio-Frontend/src/routes/project.jsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Project } from '../pages/projectPage'
-
-export const Route = createFileRoute('/project')({
-  component: Project,
-})
-
-
