7813fbc3f2aa404600820c26cb22e60e234915e0
Saimahmed <softwareengineer278@gmail.com>
2025-08-12 14:57:05 +0500

    feat(auth-backend): add endpoints for resetPass, changePass and deleteAccount

--- Portfolio-Backend/src/routes/auth.Routes.js
+++ Portfolio-Backend/src/routes/auth.Routes.js
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import {
+  accountDeletionValidators,
   forgotPassValidators,
   resendVerifcationEmailValidators,
+  changePassValidators,
+  resetPassValidators,
   userloginValidators,
   userRegistrationvalidators,
 } from "../validators/auth.validators.js";
 import validateRequest from "../middlewares/validateRequest.js";
 import {
+  changePass,
+  deleteAccount,
   forgotPass,
   loginUser,
   logOut,
   resendverificationemail,
+  resetPass,
   userRegister,
   verifyUser,
 } from "../controllers/auth.controllers.js";
@@ -35,5 +41,25 @@ router.post(
 router.post("/login", userloginValidators(), validateRequest, loginUser);
 router.get("/logOut", isloggedIn, logOut);
 router.get("/forgotPass", forgotPassValidators(), validateRequest, forgotPass);
+router.post(
+  "/resetPass/:token",
+  resetPassValidators(),
+  validateRequest,
+  resetPass,
+);
+router.post(
+  "/changePass",
+  isloggedIn,
+  changePassValidators(),
+  validateRequest,
+  changePass,
+);
+router.post(
+  "/deleteAccount",
+  isloggedIn,
+  accountDeletionValidators,
+  validateRequest,
+  deleteAccount,
+);
 
 export default router;
