7aa9571faf22f24d8d9421a52dc387fc7fe6d0f1
Saimahmed <softwareengineer278@gmail.com>
2025-08-29 15:26:30 +0500

    chore(auth-frontend): remove unused zod import from change Password route

--- Portfolio-Frontend/src/pages/auth/changePassword.jsx
+++ Portfolio-Frontend/src/pages/auth/changePassword.jsx
@@ -1,6 +1,5 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import toast from "react-hot-toast";
 import { useParams } from "react-router";
 import apiClient from "../../../service/apiClient";
