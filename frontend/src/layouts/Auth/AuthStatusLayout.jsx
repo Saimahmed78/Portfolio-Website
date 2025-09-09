df7f48cccfccd17a87481a3877fb93296ef346ee
Saimahmed <softwareengineer278@gmail.com>
2025-09-09 21:34:38 +0500

    refactor(layout): remove unnecessary wrapper div from AuthStatusLayout

--- frontend/src/layouts/Auth/AuthStatusLayout.jsx
+++ frontend/src/layouts/Auth/AuthStatusLayout.jsx
@@ -1,11 +1,9 @@
-import { Outlet } from "react-router"; // make sure to use react-router-dom
+import { Outlet } from "react-router";
 
 export default function AuthStatusLayout() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-[#0f172a] p-5 font-inter animate-fadeIn">
-      <div className="bg-[#1e293b] rounded-2xl shadow-xl flex flex-col gap-4 relative overflow-hidden w-full max-w-md p-8 text-center animate-fadeInSm">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 }
