a43296e774f683ddb74548763baece6b4dc6bf94
Saimahmed <softwareengineer278@gmail.com>
2025-09-09 07:51:39 +0500

    feat(infrastructure-backend): add DB connection and mail transporter

---
+++ backend/src/infrastructure/mail/mail.transporter.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.BREVO_SMTP_HOST,
+  port: process.env.BREVO_SMTP_PORT,
+  secure: false, // true for 465, false for 587
+  auth: {
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_PASS,
+  },
+});
+
+export const sendMail = async ({ email, subject, text, html }) => {
+  const mailOptions = {
+    from: `Portfolio Project 👻 <${process.env.FROM_EMAIL}>`,
+    to: email,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`✅ Email sent to ${email}`);
+  } catch (error) {
+    console.error("❌ Email not sent:", error);
+    throw error;
+  }
+};
